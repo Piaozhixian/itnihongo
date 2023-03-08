a9f59befb57545d562196a1c59864802d07f2a67
piao <pakushiken@gmail.com>
2023-03-09 01:10:01 +0900

    import mui & read data from firestore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,64 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {collection, getFirestore, getDocs, limit, orderBy, query, where} from "firebase/firestore";
+import {Box, Button} from '@mui/material';
+import {useState} from 'react';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
-
-
-export default function Home() {
-  // Initialize Firebase
+function Home() {
   const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app)
+
+  const [questionWord, setQuestionWord] = useState()
+  const [answer, setAnswer] = useState()
+  const [selections, setSelections] = useState<string[]>([])
+  async function getData () {
+    const wordsRef = collection(db, 'words')
+    const q = query(wordsRef, orderBy('ja'),limit(1))
+    const querySnapshot = await getDocs(q)
+    querySnapshot.forEach((doc) => {
+      setQuestionWord(doc.data().ja)
+      setAnswer(doc.data().cn)
+    })
+    const wrongSelectionsQuery = query(wordsRef,where("ja", "!=", "テスト" ),limit(3))
+    const wrongSelectionsQuerySnapshot = await getDocs(wrongSelectionsQuery)
+    const arr: string[] = []
+    wrongSelectionsQuerySnapshot.forEach((doc) => {
+      arr.push(doc.data().cn)
+    })
+    setSelections(arr)
+  }
+  getData()
+
   return (
-    <>home</>
+    <>
+      <Box>
+        {questionWord}
+      </Box>
+      <Box>
+        {
+          selections.map((selection,index)=>{
+            return (<Button key={`selection-${index}`}>{selection}</Button>)
+          })
+        }
+        <Button>{answer}</Button>
+      </Box>
+    </>
   )
 }
+
+export default Home;
