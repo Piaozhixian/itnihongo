f49dfebe34199e4f7da60b86409d3414d903b417
piao <pakushiken@gmail.com>
2023-03-13 17:58:13 +0900

    add click action & shuffle selections

--- src/components/atoms/SelectionButton.tsx
+++ src/components/atoms/SelectionButton.tsx
@@ -1,19 +1,28 @@
 import { Button } from "@mui/material";
+import { MouseEventHandler } from "react";
 
 type SelectionButtonProps = {
   /**
    * 按钮中显示的文字
    */
   text: string;
+  /**
+   * 点击按钮时的处理
+   */
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
 /**
  * 选项按钮组件
  * @param SelectionButtonProps
  */
-function SelectionButton({ text }: SelectionButtonProps) {
+function SelectionButton({ text, onClick }: SelectionButtonProps) {
   return (
-    <Button variant="outlined" style={{ fontFamily: "sans-serif" }}>
+    <Button
+      variant="outlined"
+      style={{ fontFamily: "sans-serif" }}
+      onClick={onClick}
+    >
       {text}
     </Button>
   );
--- src/components/organisms/Selections.tsx
+++ src/components/organisms/Selections.tsx
@@ -13,14 +13,46 @@ type SelectionsProps = {
 };
 
 /**
+ * 打乱选项
+ * @param selections 所有选项
+ * @return 被打乱的选项数组
+ */
+function ShuffleSelections(selections: string[]) {
+  const shuffledSelections = [];
+
+  while (selections.length > 0) {
+    const k = Math.floor(Math.random() * selections.length);
+
+    shuffledSelections.push(selections[k]);
+    selections.splice(k, 1);
+  }
+
+  return shuffledSelections;
+}
+
+/**
  * 显示所有选项组件
  * @param  SelectionsProps
  */
 function Selections({ selections, answer }: SelectionsProps) {
+  const shuffledSelections = ShuffleSelections(selections);
   return (
     <Box sx={{ width: 200, display: "flex", flexDirection: "column", gap: 1 }}>
-      {selections.map((selection, index) => {
-        return <SelectionButton text={selection} key={`selection-${index}`} />;
+      {shuffledSelections.map((selection, index) => {
+        return (
+          <SelectionButton
+            text={selection}
+            key={`selection-${index}`}
+            onClick={(e) => {
+              // TODO: 显示结果
+              if (e.currentTarget.textContent === answer) {
+                alert("正确");
+              } else {
+                alert("错误");
+              }
+            }}
+          />
+        );
       })}
     </Box>
   );
