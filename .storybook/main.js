5c9e26976739e786ed01f2487ec03833a7f62e30
piao <pakushiken@gmail.com>
2023-05-08 19:13:02 +0900

    upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,28 @@
 const path = require("path");
-
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {
+      builder: {
+        fsCache: true,
+        lazyCompilation: true
+      }
+    }
   },
-  webpackFinal: async (config) => {
+  features: {
+    storyStoreV7: true
+  },
+  webpackFinal: async config => {
     config.resolve.alias = {
       ...config.resolve.alias,
       "@": path.resolve(__dirname, "../src/"),
-      "@styles": path.resolve(__dirname, "../styles/"),
+      "@styles": path.resolve(__dirname, "../styles/")
     };
     return config;
   },
+  docs: {
+    autodocs: true
+  }
 };
